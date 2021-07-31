b24e24e96af20246a141391d8afa831d31e5caba
So-chiru <unleash9224@gmail.com>
2021-08-01 01:43:00 +0900

    Update scrollbar visibility

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 
 export enum PopupState {
   Hidden = 0,
@@ -14,7 +14,25 @@ interface PopupProps {
 
 import '@/styles/components/popup.scss'
 
+const useBodyScroll = (state: PopupState) => {
+  useEffect(() => {
+    if (state === PopupState.Full) {
+      if (document.body.style.overflow !== 'hidden') {
+        document.body.style.overflow = 'hidden'
+      }
+    } else {
+      if (document.body.style.overflow !== 'auto') {
+        document.body.style.overflow = 'auto'
+      }
+    }
+  }, [state])
+
+  return true
+}
+
 export const Popup = ({ children, minimize, state }: PopupProps) => {
+  useBodyScroll(state)
+
   return (
     <div className='popup-zone' data-state={state}>
       <div

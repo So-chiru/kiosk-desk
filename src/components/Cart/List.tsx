0544880fdc49adf78d5aea85929aa550a1dc6bd3
So-chiru <unleash9224@gmail.com>
2021-08-01 13:45:15 +0900

    Add cart animation

--- src/components/Cart/List.tsx
+++ src/components/Cart/List.tsx
@@ -4,7 +4,9 @@ interface CartListProps {
 
 import { updateCartItemCount } from '@/store/cart/actions'
 import '@/styles/components/cart/list.scss'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import CartMenuItem from './Item'
 
@@ -15,18 +17,35 @@ export const DetailedCartList = ({ items }: CartListProps) => {
     dispatch(updateCartItemCount(item.item, num))
   }
 
+  const [lastItems, setLastItems] = useState<CartItem[]>(items)
+
+  useEffect(() => setLastItems(items), [items])
+
   return (
     <div className='cart-menus'>
       {!items.length && (
         <h1 className='no-menu'>장바구니에 상품이 없습니다.</h1>
       )}
-      {...items.map(item => (
-        <CartMenuItem
-          key={item.item.id}
-          item={item}
-          updateCount={updateItemCount}
-        ></CartMenuItem>
-      ))}
+      <TransitionGroup>
+        {...items.map((item, index) => (
+          <CSSTransition
+            key={item.item.id}
+            in={
+              lastItems &&
+              items.length !== lastItems.length &&
+              (items[index].amount === items[index].amount ||
+                items[index].item.id === lastItems[index].item.id)
+            }
+            timeout={{ enter: 600, exit: 300 }}
+            classNames='kiosk-cart-item'
+          >
+            <CartMenuItem
+              item={item}
+              updateCount={updateItemCount}
+            ></CartMenuItem>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   )
 }
